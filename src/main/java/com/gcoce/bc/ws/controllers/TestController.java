0f9a6a2f3bebfaf16861851841b3ca0b802843ff
gcoce <gabrielcoce@outlook.com>
2023-05-05 16:08:15 -0600

    feat: se modifica respuesta de test

--- src/main/java/com/gcoce/bc/ws/controllers/TestController.java
+++ src/main/java/com/gcoce/bc/ws/controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.gcoce.bc.ws.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,36 +12,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
     @GetMapping("/all")
-    public String allAccess() {
-        return "Public Content.";
+    public ResponseEntity<?> allAccess() {
+        return ResponseEntity.ok("Servidor funcionado correctamente");
     }
 
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN')")
-    public String adminAccess() {
-        return "Admin Content.";
+    public ResponseEntity<?> adminAccess() {
+        return ResponseEntity.ok("Admin Content.");
     }
+
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER')")
-    public String userAccess() {
-        return "User Content.";
+    public ResponseEntity<?> userAccess() {
+        return ResponseEntity.ok("User Content.");
     }
 
     @GetMapping("/beneficio")
     @PreAuthorize("hasRole('BENEFICIO')")
-    public String beneficioAccess() {
-        return "Beneficio Content.";
+    public ResponseEntity<?> beneficioAccess() {
+        return ResponseEntity.ok("Beneficio Content.");
     }
 
     @GetMapping("/agricultor")
     @PreAuthorize("hasRole('AGRICULTOR')")
-    public String agricultorAccess() {
-        return "Agricultor Content.";
+    public ResponseEntity<?> agricultorAccess() {
+        return ResponseEntity.ok("Agricultor Content.");
     }
 
     @GetMapping("/peso-cabal")
     @PreAuthorize("hasRole('PESO_CABAL')")
-    public String pesoCabalAccess() {
-        return "Peso Cabal Content.";
+    public ResponseEntity<?> pesoCabalAccess() {
+        return ResponseEntity.ok("Peso Cabal Content.");
     }
 }
